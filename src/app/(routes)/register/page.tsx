65a6b459340cee50162222862fc2ab9a8e766420
inakicarcereny <inakicarce@hotmail.com.ar>
2024-12-19 13:21:32 -0300

    Add register page component with layout and design; enhance user interface with a structured registration form

---
+++ src/app/(routes)/register/page.tsx
@@ -0,0 +1,19 @@
+import { Netflix } from '@/icons';
+
+export default function Register() {
+  return (
+    <section className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('/hero.jpg')] relative">
+      <Netflix className="absolute left-36 z-50" />
+
+      <div className="flex flex-col items-center justify-center absolute inset-0">
+        <div className="w-[500px] h-[750px] bg-black bg-opacity-90 flex flex-col p-16 z-50">
+          <h2 className="text-white font-semibold text-3xl">Register</h2>
+
+          <form className="flex flex-col gap-4"></form>
+        </div>
+      </div>
+
+      <div className="absolute bg-black bg-opacity-80 w-full h-full top-0 left-0 z-0"></div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, Reasons, Trending } from '@/components';
+import { Hero, Reasons, Trending, Frequently, Footer } from '@/components';
 
 export default function Home() {
   return (
@@ -8,6 +8,10 @@ export default function Home() {
       <Trending />
 
       <Reasons />
+
+      <Frequently />
+
+      <Footer />
     </>
   );
 }
