3671b172fb8badb56c423ff82ce5c64c6f3e120a
inakicarcereny <inakicarce@hotmail.com.ar>
2024-12-19 11:30:37 -0300

    Add Reasons component and reasons data structure with relevant details and icons for display purposes

---
+++ src/app/components/reasons/Reasons.tsx
@@ -0,0 +1,3 @@
+export function Reasons() {
+  return <section></section>;
+}
---
+++ src/app/lib/reasons.ts
@@ -0,0 +1,21 @@
+import { Tv, ArrowDown, Watcher } from '@/icons';
+
+export const REASONS = [
+  {
+    title: 'Enjoy on your TV',
+    resume:
+      'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.',
+    icon: Tv(),
+  },
+  {
+    title: 'Download your shows to watch offline',
+    resume: 'Save your favorites easily and always have something to watch.',
+    icon: ArrowDown(),
+  },
+  {
+    title: 'Watch everywhere',
+    resume:
+      'Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.',
+    icon: Watcher,
+  },
+];
